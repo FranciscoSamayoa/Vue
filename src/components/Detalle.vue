076cd015081e3722fd13f5065ce0b991922c38b5
FranciscoSamayoa <113380312+FranciscoSamayoa@users.noreply.github.com>
2022-10-15 15:20:47 -0500

    first commit

---
+++ src/components/Detalle.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <h1>Detalle</h1>
+      <br>
+      <label>Editar: </label>
+      <input v-model="objeto.title" placeholder="editeme" class="col-lg-6">
+      <br>
+      <table class="table thead-dark ">
+        <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Titulo</th>
+          <th scope="col">Body</th>
+
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>{{ objeto?.id }}</td>
+          <td>{{ objeto?.title }}</td>
+          <td>{{ objeto?.body }}</td>
+
+        </tr>
+        </tbody>
+      </table>
+      <br>
+      <br>
+      <b-button to="/" class="btn btn-info"> Inicio </b-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'detalle-info',
+    props: {
+        id: Number
+    },
+  data(){
+      return{
+        idObjeto: Number,
+        objeto: null
+      }
+  },
+  created() {
+      this.idObjeto = this.$route.params.id;
+      this.obtenerDetalle();
+  },
+  methods:{
+      obtenerDetalle(){
+        this.$http.get("https://jsonplaceholder.typicode.com/posts/" + this.idObjeto)
+            .then((response)=>{
+              this.objeto = response.data;
+
+              console.log(response);
+            })
+      }
+  }
+
+}
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,39 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>Proyecto Final</h1>
+    <br>
+    <h1>Francisco Samayoa Carmona</h1>
+    <br>
+    <!--
+       <h1 v-if="author.name == 'Oscar'">Eso es correcto</h1>
+      <h1 v-else>Esta mal el author</h1>
+      <br>
+      <br>
+    -->
+  
+  <table class="table thead-dark">
+  <thead>
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">Titulo</th>
+      <th scope="col">Body</th>
+      <th scope="col">Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="posts in listaPosts" :key="posts.id">
+      <td>{{ posts.id }}</td>
+      <td>{{ posts.title }}</td>
+      <td>{{ posts.body }}</td>
+      <td> 
+        <b-button :to="'/detalle/' + posts.id" class="btn btn-info"> Ver mas </b-button>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+
   </div>
 </template>
 
@@ -34,25 +41,50 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    msg: String,
+    id: Number,
+  },
+  data(){
+    return {
+      author: {
+        name: 'Jhon Doe',
+        books: [
+          'Vue 2 - Advanced Guide',
+          'Vue 3 - Basic Guide',
+          'Vue 4 - The Mystery'
+        ]
+      },
+      carros: {
+        name: null,
+        colores: []
+      },
+      listaPosts: [],
+    }
+  },
+
+  created(){
+    this.obtenerApi();
+  },
+
+  methods:{
+    obtenerApi(){
+      let apiURL = 'https://jsonplaceholder.typicode.com/posts';
+      this.listaPosts= [];
+
+      this.$http.get(apiURL).then(response => {
+        if(response.status == 200){
+        let data = response.data;
+
+        data.forEach((element, index) =>{
+          if (index + 1  <= 30){
+            this.listaPosts.push(element)
+          }
+        });
+        }
+      })
+      .catch(e => console.log(e))
+      .finally(()=>console.log("Error"))
+    }
+  },
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
